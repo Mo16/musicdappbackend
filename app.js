8a3df2da79b6ecbfddcd6a75aa235963e77fabe6
moerefine <moe@refine.agency>
2024-01-13 10:33:08 +0000

    new

--- app.js
+++ app.js
@@ -7,19 +7,55 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/tts", async (req, res) => {
-  console.log("hworking")
+    console.log("hworking");
     const { lyrics } = req.body;
 
     const uberduckOptions = {
         method: "POST",
         headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          authorization: 'Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg='
+            accept: "application/json",
+            "content-type": "application/json",
+            authorization:
+                "Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg=",
+        },
+        body: JSON.stringify({
+            voicemodel_uuid: "5e510987-5353-4598-9d48-2bc0e23967e6",
+            format: "json",
+            backing_track: "726f4142-c85a-4afc-a1e8-e76342692329",
+            lyrics,
+        }),
+    };
+
+    try {
+        const response = await fetch(
+            "https://api.uberduck.ai/tts/freestyle",
+            uberduckOptions
+        );
+        console.log(33, response);
+        const data = await response.json();
+        res.json(data);
+        console.log(data);
+    } catch (error) {
+        console.log(38, error);
+        res.status(500).send("Error forwarding request to Uberduck API");
+    }
+});
+
+app.post("/audio", async (req, res) => {
+    console.log("hworking");
+    const { lyrics } = req.body;
+
+    const uberduckOptions = {
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            "content-type": "application/json",
+            authorization:
+                "Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg=",
         },
         body: JSON.stringify({
             bpm: 90,
-            voice: "zwf",
+            voicemodel_uuid: "5e510987-5353-4598-9d48-2bc0e23967e6",
             format: "json",
             lyrics,
         }),
@@ -30,16 +66,15 @@ app.post("/tts", async (req, res) => {
             "https://api.uberduck.ai/tts/freestyle",
             uberduckOptions
         );
-        console.log(33, response)
+        console.log(33, response);
         const data = await response.json();
         res.json(data);
         console.log(data);
     } catch (error) {
-      console.log(38, error)
+        console.log(38, error);
         res.status(500).send("Error forwarding request to Uberduck API");
     }
 });
-
 
 app.post("/prompt", async (req, res) => {
     console.log("1");
@@ -48,9 +83,10 @@ app.post("/prompt", async (req, res) => {
     const uberduckOptions = {
         method: "POST",
         headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          authorization: 'Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg='
+            accept: "application/json",
+            "content-type": "application/json",
+            authorization:
+                "Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg=",
         },
         body: JSON.stringify({
             subject,
@@ -70,14 +106,15 @@ app.post("/prompt", async (req, res) => {
         const uberduckOptions2 = {
             method: "POST",
             headers: {
-              accept: 'application/json',
-              'content-type': 'application/json',
-              authorization: 'Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg='
+                accept: "application/json",
+                "content-type": "application/json",
+                authorization:
+                    "Basic cHViX3FoZm1jcWZ1ZWFmZHBoYnFzaDpwa185MjMxMGEyZC02NzZlLTQzY2UtYmVjMC0yOGM0YWU4Y2I1ODg=",
             },
             body: JSON.stringify({
-                bpm: 90,
-                voice: "zwf",
+                voicemodel_uuid: "5e510987-5353-4598-9d48-2bc0e23967e6",
                 format: "json",
+                backing_track: "726f4142-c85a-4afc-a1e8-e76342692329",
                 lyrics: data.lyrics,
             }),
         };
@@ -88,14 +125,17 @@ app.post("/prompt", async (req, res) => {
                 uberduckOptions2
             );
             const data = await response.json();
-            console.log(data)
+            console.log(data);
 
             res.json(data);
             console.log(data);
         } catch (error) {
+            console.log(error);
             res.status(500).send("Error forwarding request to Uberduck API");
         }
     } catch (error) {
+        console.log(error);
+
         res.status(500).send("Error forwarding request to Uberduck API");
     }
 });
